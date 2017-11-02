bba674075da3a5ea0ec004255a93595a2c9fbedf
Tiia Schurig <tiia_schurig@aiga.org>
2017-11-01 20:59:14 -0400

    final code

--- script.js
+++ script.js
@@ -1,10 +1,55 @@
-var lemQuant = Cookies.get("lemon");
-if (lemQuant) {
-  alert("you ordered a lemon cookie!");
+function setLemonCookie () {
+  // grab current cookie value
+  // if undefined
+  // set it to 1
+  // otherwise
+  // add 1 to original value and set it
+  var lemCookie = Cookies.get("lemon");
+  if (lemCookie=== undefined) {
+    Cookies.set("lemon", 1);
+  } else {
+    lemCookie = parseInt(lemCookie);
+    Cookies.set("lemon", lemCookie + 1);
+  }
+}
+document.getElementById("lemon-order").addEventListener("click", setLemonCookie);
+
+function setSugarCookie () {
+  // grab current cookie value
+  // if undefined
+  // set it to 1
+  // otherwise
+  // add 1 to original value and set it
+  var sugCookie = Cookies.get("sugar");
+  if (sugCookie=== undefined) {
+    Cookies.set("sugar", 1);
+  } else {
+    sugCookie = parseInt(sugCookie);
+    Cookies.set("sugar", sugCookie + 1);
+  }
+}
+document.getElementById("sugar-order").addEventListener("click", setSugarCookie);
+
+function setChocoCookie () {
+  // grab current cookie value
+  // if undefined
+  // set it to 1
+  // otherwise
+  // add 1 to original value and set it
+  var chocoCookie = Cookies.get("chocolate");
+  if (chocoCookie=== undefined) {
+    Cookies.set("chocolate", 1);
+  } else {
+    chocoCookie = parseInt(chocoCookie);
+    Cookies.set("chocolate", chocoCookie + 1);
+  }
+}
+document.getElementById("chocolate-order").addEventListener("click", setChocoCookie);
+
+function deleteOrder () {
+  Cookies.set("chocolate", 0);
+  Cookies.set("sugar", 0);
+  Cookies.set("lemon", 0);
 }
 
-function setCookie () {
-  var lemQuant = document.getElementById("lemon-order").value;
-  Cookies.set("lemon", lemQuant);
-}
-document.getElementById("lemon-order").addEventListener("click", setCookie);
+document.getElementById("delete").addEventListener("click", deleteOrder);
